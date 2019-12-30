b5a51084c62ff9cf261851cb9699a5c97c9a6f95
David Prandzioch <hello+git@davd.eu>
2019-12-30 12:04:17 +0100

    Gofmt the project

--- rest-api/request_handler.go
+++ rest-api/request_handler.go
@@ -1,86 +1,86 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net"
-    "net/http"
-    "strings"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
 
-    "dyndns/ipparser"
+	"dyndns/ipparser"
 )
 
 type RequestDataExtractor struct {
-    Address func (request *http.Request) string
-    Secret func (request *http.Request) string
-    Domain func (request *http.Request) string
+	Address func(request *http.Request) string
+	Secret  func(request *http.Request) string
+	Domain  func(request *http.Request) string
 }
 
 type WebserviceResponse struct {
-    Success bool
-    Message string
-    Domain string
-    Domains []string
-    Address string
-    AddrType string
+	Success  bool
+	Message  string
+	Domain   string
+	Domains  []string
+	Address  string
+	AddrType string
 }
 
 func BuildWebserviceResponseFromRequest(r *http.Request, appConfig *Config, extractors RequestDataExtractor) WebserviceResponse {
-    response := WebserviceResponse{}
+	response := WebserviceResponse{}
 
-    sharedSecret := extractors.Secret(r)
-    response.Domains = strings.Split(extractors.Domain(r), ",")
-    response.Address = extractors.Address(r)
+	sharedSecret := extractors.Secret(r)
+	response.Domains = strings.Split(extractors.Domain(r), ",")
+	response.Address = extractors.Address(r)
 
-    if sharedSecret != appConfig.SharedSecret {
-        log.Println(fmt.Sprintf("Invalid shared secret: %s", sharedSecret))
-        response.Success = false
-        response.Message = "Invalid Credentials"
-        return response
-    }
+	if sharedSecret != appConfig.SharedSecret {
+		log.Println(fmt.Sprintf("Invalid shared secret: %s", sharedSecret))
+		response.Success = false
+		response.Message = "Invalid Credentials"
+		return response
+	}
 
-    for _, domain := range response.Domains {
-        if domain == "" {
-            response.Success = false
-            response.Message = fmt.Sprintf("Domain not set")
-            log.Println("Domain not set")
-            return response
-        }
-    }
+	for _, domain := range response.Domains {
+		if domain == "" {
+			response.Success = false
+			response.Message = fmt.Sprintf("Domain not set")
+			log.Println("Domain not set")
+			return response
+		}
+	}
 
-    // kept in the response for compatibility reasons
-    response.Domain = strings.Join(response.Domains, ",")
+	// kept in the response for compatibility reasons
+	response.Domain = strings.Join(response.Domains, ",")
 
-    if ipparser.ValidIP4(response.Address) {
-        response.AddrType = "A"
-    } else if ipparser.ValidIP6(response.Address) {
-        response.AddrType = "AAAA"
-    } else {
-        ip, _, err := net.SplitHostPort(r.RemoteAddr)
-        
-        if err != nil {
-            response.Success = false
-            response.Message = fmt.Sprintf("%q is neither a valid IPv4 nor IPv6 address", r.RemoteAddr)
-            log.Println(fmt.Sprintf("Invalid address: %q", r.RemoteAddr))
-            return response
-        }
-        
-        // @todo refactor this code to remove duplication
-        if ipparser.ValidIP4(ip) {
-            response.AddrType = "A"
-            response.Address = ip
-        } else if ipparser.ValidIP6(ip) {
-            response.AddrType = "AAAA"
-            response.Address = ip
-        } else {
-            response.Success = false
-            response.Message = fmt.Sprintf("%s is neither a valid IPv4 nor IPv6 address", response.Address)
-            log.Println(fmt.Sprintf("Invalid address: %s", response.Address))
-            return response
-        }
-    }
+	if ipparser.ValidIP4(response.Address) {
+		response.AddrType = "A"
+	} else if ipparser.ValidIP6(response.Address) {
+		response.AddrType = "AAAA"
+	} else {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 
-    response.Success = true
+		if err != nil {
+			response.Success = false
+			response.Message = fmt.Sprintf("%q is neither a valid IPv4 nor IPv6 address", r.RemoteAddr)
+			log.Println(fmt.Sprintf("Invalid address: %q", r.RemoteAddr))
+			return response
+		}
 
-    return response
+		// @todo refactor this code to remove duplication
+		if ipparser.ValidIP4(ip) {
+			response.AddrType = "A"
+			response.Address = ip
+		} else if ipparser.ValidIP6(ip) {
+			response.AddrType = "AAAA"
+			response.Address = ip
+		} else {
+			response.Success = false
+			response.Message = fmt.Sprintf("%s is neither a valid IPv4 nor IPv6 address", response.Address)
+			log.Println(fmt.Sprintf("Invalid address: %s", response.Address))
+			return response
+		}
+	}
+
+	response.Success = true
+
+	return response
 }
